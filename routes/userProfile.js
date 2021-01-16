8febf4b635db4266b1456b51fa6a8cf0535a3862
siddharthkoli <siddharthkoli2401@gmail.com>
2021-01-16 22:28:12 +0530

    added updatePassword

--- routes/userProfile.js
+++ routes/userProfile.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const sql = require('mssql');
+const bcrypt = require('bcrypt');
 const { connectionString } = require('../dev-config');
 const SQLServerConnectionString = 'Server=DESKTOP-VBR6T2V\\SQLEXPRESS;Database=testPCDB;User Id=sa;Password=Svk2432k01;';
 // const SQLServerConnectionString = connectionString;
@@ -40,6 +41,42 @@ router.patch('/updateProfile', async (req, res) => {
     } catch (e) {
         sql.close();
         console.log(`[updateProfile|userProfile.js] ${e}`);
+        res.sendStatus(500);
+    }
+});
+
+router.patch('/updatePassword', async (req, res) => {
+    const { email, oldPassword, newPassword } = req.body;
+    try {
+        let query = `SELECT Password FROM Users WHERE Email='${email}'`;
+        await sql.connect(SQLServerConnectionString);
+        let result = await sql.query(query);
+        if (result.recordset.length === 0)
+            return res.sendStatus(401);
+        const match = await bcrypt.compare(oldPassword, result.recordset[0].Password);
+        if (match) {
+            bcrypt.genSalt(10, (err, salt) => {
+                if (err) {
+                    console.log(`[userProfile|updatePassword (1st log)] ${err}`);
+                    return res.sendStatus(500);
+                }
+                bcrypt.hash(newPassword, salt, async (err, hash) => {
+                    if (err) {
+                        console.log(`[userProfile|updatePassword (2st log)] ${err}`);
+                        return res.sendStatus(500);
+                    }
+                    query = `UPDATE Users
+                    SET Password='${hash}'
+                    WHERE Email='${email}'`;
+                    await sql.query(query);
+                    sql.close();
+                    return res.sendStatus(200);
+                });
+            });
+        }
+    } catch (e) {
+        sql.close();
+        console.log(`[userProfile.js|updatePassword (3rd log)] ${err}`);
         res.sendStatus(500);
     }
 });
